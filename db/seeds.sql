1291cb84c33f0b69ce52dabccaf5c1d85222c511
rawanfatima <rawanfatima@outlook.com>
2021-09-27 13:45:40 -0700

    added schema/seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates(first_name, last_name, industry_connected)
+VALUES
+('Elena', 'Gilbert', 1),
+('Bonnie', 'Bennett',1),
+('Caroline', 'Forbes', 0),
+('Katherine', 'Pierce', 1),
+('Kara', 'Danvers', 1),
+('Alex', 'Danvers', 0),
+('Win', 'Schott', 0),
+('James', 'Olsen', 1),
+('Lena', 'Luthor', 1),
+('Samantha', 'Arias', 1);
