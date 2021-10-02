2ade46c55b2cdc99a70f1e6a545aca1ae060c01e
rawanfatima <rawanfatima@outlook.com>
2021-10-01 20:21:12 -0700

    merged party/candidates tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,18 @@
-INSERT INTO candidates(first_name, last_name, industry_connected)
+INSERT INTO parties (name, description)
 VALUES
-('Elena', 'Gilbert', 1),
-('Bonnie', 'Bennett',1),
-('Caroline', 'Forbes', 0),
-('Katherine', 'Pierce', 1),
-('Kara', 'Danvers', 1),
-('Alex', 'Danvers', 0),
-('Win', 'Schott', 0),
-('James', 'Olsen', 1),
-('Lena', 'Luthor', 1),
-('Samantha', 'Arias', 1);
+  ('Candor', 'The Honest'),
+  ('Dauntless', 'The Brave'),
+  ('Erudite','The Intelligent');
+  
+  INSERT INTO candidates(first_name, last_name, party_id, industry_connected)
+VALUES
+('Elena', 'Gilbert', 1, 1),
+('Bonnie', 'Bennett', 1, 1),
+('Caroline', 'Forbes', 1, 0),
+('Katherine', 'Pierce', 2, 1),
+('Kara', 'Danvers', 2, 1),
+('Alex', 'Danvers', 3, 0),
+('Win', 'Schott', 3, 0),
+('James', 'Olsen', 3, 1),
+('Lena', 'Luthor', 3, 1),
+('Samantha', 'Arias', NULL, 1);
--- server.js
+++ server.js
@@ -42,10 +42,16 @@ const db = new sqlite3.Database('./db/election.db', (err) => {
 //     console.log(result, this.lastID);
 // });
 
+
+
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
       if (err) {
@@ -60,9 +66,13 @@ app.get('/api/candidate/:id', (req, res) => {
     });
   });
 
-//GET All candidate
+//GET All candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if(err) {
@@ -118,10 +128,90 @@ db.run(sql, params, function(err, result) {
 });
   });
 
+
+//PUT (Update) a candidate
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+
+if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
+  //GET all parties
+  app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+//GET a single party
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+//DELETE a party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({ message: 'successfully deleted', changes: this.changes });
+  });
+});
+
+
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
 });
+
+
+
 
 
 //start server after the db connection
